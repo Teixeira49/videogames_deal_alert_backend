6da4aa995a392f5ef863f75f6967a012d49bac32
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-01 18:24:40 -0400

    Refactor constants in Constants class to include status codes and messages

--- api/utils/constants/app_constants.py
+++
@@ -1,8 +0,0 @@
- 
-class AppConstants:
-
-    STEAM_SHOP_ID = 61
-
-    EPIC_GAMES_SHOP_ID = 16
-
-    PRICE_KEY = 'price'
--- api/utils/constants/constants.py
+++ api/utils/constants/constants.py
@@ -1,14 +1,24 @@
-import os
-
 class Constants:
 
-    API_KEY = os.getenv("SITE_KEY")
-
-    CLIENT_ID = os.getenv("SITE_CLIENT_ID")
-
-    BASE_URL = os.getenv("SITE_URL")
-
-    SITE_CLIENT_SECRET = os.getenv("SITE_CLIENT_SECRET")
-
     PAGE_LIMIT = 10
+
+    STEAM_SHOP_ID = 61
+
+    EPIC_GAMES_SHOP_ID = 16
+
+    PRICE_KEY = 'price'
+
+    STATUS_OK = 200
+
+    STATUS_OK_MSG = 'Success'
+
+    STATUS_OK_DEATILS = 'Elemento retornado con exito'
+
+    STATUS_NOT_FOUND = 404
+
+    STATUS_NOT_FOUND_MSG = 'Not Found'
+
+    STATUS_INTERNAL_SERVER_ERROR = 500
+
+    STATUS_INTERNAL_SERVER_ERROR_MSG = 'Internal Server Error'
 
