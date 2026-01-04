78da762267f27d7256e13ea0197992a51ac4a6d2
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-04 01:32:19 -0400

    Refactor config settings: add SMTP configuration and ensure proper environment variable usage

--- api/core/config/config.py
+++ api/core/config/config.py
@@ -9,3 +9,11 @@ class Config:
     BASE_URL = os.getenv("SITE_URL")
 
     SITE_CLIENT_SECRET = os.getenv("SITE_CLIENT_SECRET")
+
+    SMTP_PORT = 587
+
+    SMTP_SERVER = os.getenv("SMTP_SERVER")
+
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+    EMAIL_SENDER = os.getenv("EMAIL_SENDER")
